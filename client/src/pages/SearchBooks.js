938b8c177c738a50a9446b3ac8fed1a5173cfb20
daevidvo <itsdavidvo@gmail.com>
2023-06-02 23:21:30 -0700

    added README along with TODO.txt for reference later

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -62,6 +62,10 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
+    console.log(bookToSave)
+
+
+
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -70,7 +74,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const {data} = await saveBook(bookToSave, token);
+      const {data} = await saveBook(bookToSave);
+
+      console.log(data)
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
